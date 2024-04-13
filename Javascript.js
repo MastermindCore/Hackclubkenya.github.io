a043c532660a1a93b7718916e526e5ef21dd557f
Ram Ratan <84262908+Hackclubkenya@users.noreply.github.com>
2024-04-13 16:29:06 +0300

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const loadingScreen = document.getElementById("loadingScreen");
+
+  // Show loading screen
+  loadingScreen.classList.remove("hidden");
+
+  // Hide loading screen when page has fully loaded
+  window.addEventListener("load", function() {
+    loadingScreen.classList.add("hidden");
+  });
+});
 
